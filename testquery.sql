58e0560e50a0233c38c199a4e18780e7157e8457
TylerDickardMMO <107117271+TylerDickardMMO@users.noreply.github.com>
2022-07-26 16:37:49 -0400

    Delete testquery.sql

--- testquery.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	SYS.Name0 AS Name,
-	InstallSoftware.ProductName0 AS ProductName,
-	InstallSoftware.ProductVersion0 AS ProductVersion
-FROM
-	v_R_System AS SYS
-
-JOIN v_GS_INSTALLED_SOFTWARE InstallSoftware ON InstallSoftware.ResourceID = SYS.ResourceID
