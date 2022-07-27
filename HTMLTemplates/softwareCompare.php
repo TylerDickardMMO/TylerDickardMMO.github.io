c9c614cc5bbf332f1f036df5d79397c166c04de1
TylerDickardMMO <107117271+TylerDickardMMO@users.noreply.github.com>
2022-07-27 10:54:26 -0400

    V0.1.4.6 Turned the Query into a readable array
    Implemented a change where the query is stored in a readable js array

--- HTMLTemplates/softwareCompare.php
+++ HTMLTemplates/softwareCompare.php
@@ -1,20 +1,28 @@
+<script>
 <?php
-include_once('../includes/dbh.inc.php');
+    include_once("../includes/dbh.inc.php");
     $oldDevName = $_COOKIE['oldModelNumber'];
+    $js_array = array();
     $sql = "
     SELECT 
-	    SYS.Name0 AS Name,
-	    InstallSoftware.ProductName0 AS ProductName,
-	    InstallSoftware.ProductVersion0 AS ProductVersion
+        SYS.Name0 AS Name,
+        InstallSoftware.ProductName0 AS ProductName,
+        InstallSoftware.ProductVersion0 AS ProductVersion
     FROM
-	    v_R_System AS SYS
+        v_R_System AS SYS
     JOIN v_GS_INSTALLED_SOFTWARE InstallSoftware ON InstallSoftware.ResourceID = SYS.ResourceID
     WHERE SYS.Name0= ?";
     $stmt = sqlsrv_prepare($conn, $sql, array(&$oldDevName));
     sqlsrv_execute($stmt);
-    while($row = sqlsrv_fetch_array($stmt)) {
-        echo print_r($row);       // Print the entire row data
+    while ($row = sqlsrv_fetch_array($stmt)) {
+        $js_array[] = json_encode($row);
     }
+    echo "var javascript_array = ". json_encode($js_array) . ";\n";
+    
+    
     
 ?>
-
+for(let i = 0; i < javascript_array.length; i++) {
+    console.log (javascript_array[i]);
+}
+</script>
